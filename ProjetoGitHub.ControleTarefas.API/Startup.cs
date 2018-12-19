4063bb20d4c1d37a7a8ee0c52051c12c818e48f6
wagnermcarvalho <wagner_mc@terra.com.br>
2018-12-18 22:27:17 -0200

    Teste 123

--- ProjetoGitHub.ControleTarefas.API/Startup.cs
+++ ProjetoGitHub.ControleTarefas.API/Startup.cs
@@ -27,7 +27,7 @@ namespace ProjetoGitHub.ControleTarefas.API
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World! TEste 123");
             });
         }
     }
